77dc31e7978ac027892a68b03dc0047037ac9ff7
elias <elias.jaeger2004@gmail.com>
2025-03-26 19:58:01 +0100

    change

--- AufseherServer/Program.cs
+++ AufseherServer/Program.cs
@@ -1,26 +1,28 @@
 namespace AufseherServer
 {
-    public class Program
-    {
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			IHost host = CreateHostBuilder(args).Build();
+			host.Run();
+		}
 
-        public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
-
-        private static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration((hostingContext, config) =>
-                {
+		private static IHostBuilder CreateHostBuilder(string[] args) =>
+			Host.CreateDefaultBuilder(args)
+				.ConfigureAppConfiguration((hostingContext, config) =>
+				{
 #if DEBUG
-                    config.AddJsonFile("appsettings.Development.json", optional: true, reloadOnChange: true);
+					config.AddJsonFile("appsettings.Development.json", optional: true, reloadOnChange: true);
 #else
                     config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
 #endif
-
-                    config.AddEnvironmentVariables();
-                })
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseUrls("http://0.0.0.0:80", "https://0.0.0.0:443");
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+					config.AddEnvironmentVariables();
+				})
+				.ConfigureWebHostDefaults(webBuilder =>
+				{
+					webBuilder.UseUrls("http://0.0.0.0:80", "https://0.0.0.0:443");
+					webBuilder.UseStartup<Startup>();
+				});
+	}
 }
